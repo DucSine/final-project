ad2be65e80e3ab303f176ba4f833f30c486b6538
DucSine <duchoangvo97@gmail.com>
2021-04-05 21:14:53 +0700

    make it better

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+require('dotenv').config()
 const express = require('express')
-const app = express()
+const bodyParser = require('body-parser')
 
+
+const app = express()
 const port = process.env.PORT||3000
+const connectDB = require('./config/db')
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
+connectDB()
 
 app.use('/',(req,res)=> res.send('hello'))
 
