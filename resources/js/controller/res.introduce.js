bdbbfee66be4ee060a04028fe20e11a702e2c46e
DucSine <duchoangvo97@gmail.com>
2021-06-09 17:51:28 +0700

    duc commit

--- resources/js/controller/res.introduce.js
+++ resources/js/controller/res.introduce.js
@@ -82,3 +82,33 @@ window.onclick = function (e) {
 
     }
 }
+
+// load banner
+function readBannerURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        
+        reader.onload = function (e) {
+            $('#regis_show_image').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+$("#regis_image").change(function(){
+    readBannerURL(this);
+});
+
+// load image Product
+function readImageURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+        
+        reader.onload = function (e) {
+            $('#ap_show_image').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+    }
+}
+$("#regis_image").change(function(){
+    readImageURL(this);
+});
